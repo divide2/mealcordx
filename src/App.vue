9ff181aeb69c94cb9e74f78219f325d583a30021
ctt123 <283569035@qq.com>
2018-08-29 10:12:24 +0800

    安装flyio

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <script>
+var Fly = require('flyio')
+var fly = new Fly()
+
 export default {
   created () {
     // 调用API从本地缓存中获取数据
@@ -7,25 +10,42 @@ export default {
     wx.setStorageSync('logs', logs)
 
     console.log('app created and cache logs by setStorageSync')
+    wx.login({
+      success: res => {
+        if (res.code) {
+          console.log(res)
+          wx.request({
+            url: 'https://bvvy.ngrok.xiaomiqiu.cn/login/mp',
+            data: {code: res.code},
+            method: 'POST',
+            success (data) { console.log(data) }
+          })
+        } else {
+          console.log('登录失败')
+        }
+      }
+    })
+    fly.get('https://bvvy.ngrok.xiaomiqiu.cn/v1/product/find').then((res) => { console.log(res) })
   }
 }
 </script>
 
 <style>
-.container {
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  padding: 200rpx 0;
-  box-sizing: border-box;
-}
-/* this rule will be remove */
-* {
-  transition: width 2s;
-  -moz-transition: width 2s;
-  -webkit-transition: width 2s;
-  -o-transition: width 2s;
-}
+  .container {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    padding: 200 rpx 0;
+    box-sizing: border-box;
+  }
+
+  /* this rule will be remove */
+  * {
+    transition: width 2s;
+    -moz-transition: width 2s;
+    -webkit-transition: width 2s;
+    -o-transition: width 2s;
+  }
 </style>
